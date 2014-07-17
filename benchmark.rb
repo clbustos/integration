7af8fddb8a430152b4ee7deac3b45b4ecb849fc2
Rajat Kapoor <rajat100493@gmail.com>
2014-07-17 20:46:43 +0200

    One more function added, will add random fucntions, now looking at modules for accuracy

--- benchmark.rb
+++ benchmark.rb
@@ -18,3 +18,18 @@ Benchmark.bm do |x|
   #x.report("Monte Carlo:")		{Integration.integrate(0,1,{:method=>:monte_carlo},&f1)}
 end
 
+f2 = lambda{|x| 37*x**3 + 43*x**2 + x }
+puts "f(x) =  37x^3 + 43x^2 + x"
+Benchmark.bm do |x|
+  x.report("Rectangle: 			")		{Integration.integrate(0,1,{:method=>:rectangle},&f1)}
+  x.report("Trapezoid: 			")		{Integration.integrate(0,1,{:method=>:trapezoid},&f2)}
+  x.report("Simpson: 			")			{Integration.integrate(0,1,{:method=>:simpson},&f2)}
+  x.report("Simpson 3/8: 		")		{Integration.integrate(0,1,{:method=>:simpson3by8},&f2)}
+  x.report("Boole: 				")			{Integration.integrate(0,1,{:method=>:boole},&f2)}
+  x.report("Open Trapezoid: 	")  	{Integration.integrate(0,1,{:method=>:open_trapezoid},&f2)}
+  x.report("Milne: 				")  			{Integration.integrate(0,1,{:method=>:milne},&f2)}
+  x.report("Adaptive Quadrature:")  {Integration.integrate(0,1,{:method=>:adaptive_quadrature},&f2)}
+  x.report("Gauss: 				")			{Integration.integrate(0,1,{:method=>:gauss},&f2)}
+  x.report("Gauss Kronrod: 		")	{Integration.integrate(0,1,{:method=>:gauss_kronrod},&f2)}
+  x.report("Romberg: 			")			{Integration.integrate(0,1,{:method=>:romberg},&f2)}
+end
