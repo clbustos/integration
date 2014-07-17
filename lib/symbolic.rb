d54d1bf02884a3362e5c074b2cb8924bcf8742d7
Rajat Kapoor <rajat100493@gmail.com>
2014-07-17 19:05:51 +0200

    idea by carlos

--- lib/symbolic.rb
+++ lib/symbolic.rb
@@ -16,6 +16,13 @@ class Var #variable and symbol are already class in ruby
   	puts @sym
   end
 end
+
+class Polynomial
+	def initialize(poly, vars)
+		@poly = poly # Write parser? or ask the user to write full poly?
+		
+
+	end
 #java_import Java::OrgJscienceMathematicsFunction.Variable
 #java_import Java::OrgJscienceMathematicsFunction.Polynomial
 #import 'org.jscience.mathematics.number.Real'
@@ -34,3 +41,15 @@ end
 #integrating with respect to variable 'y'
 #puts fx.integrate(varY)
 
+#def polynomial(&block)
+ # varX = Variable::Local.new('x')
+ # x = Polynomial.valueOf(Real::ONE,varX)
+  yield x 
+#end
+ 
+# Creating a polynomial fx = 1(x^5) + 6(x^2).
+#fx = polynomial do |x|
+#  x.pow(5).plus(x.pow(2).times(Real.valueOf(6)))
+# end
+# Integrating with respect to variable 'x'.
+#puts fx.integrate(varX)  
