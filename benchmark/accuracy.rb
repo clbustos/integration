f130d8dabf6583b2881afdb2c716531d65196218
Rajat Kapoor <rajat100493@gmail.com>
2014-07-28 01:54:51 +0530

    added ruby methods

--- benchmark/accuracy.rb
+++ benchmark/accuracy.rb
@@ -4,14 +4,14 @@ require 'integration'
 require 'text-table'
 
 # put the funtion you want to benchmark here
-func = lambda{|x| x**2}
+func = lambda{|x| x}
 #put the actual result of the integration here
-actual_result = 1/3.0
+actual_result = 5/2.0 + 2* Math::sin(1)
 
 table = Text::Table.new
 table.head = ['Method','Result','Actual Result','Error','Accuracy']
 
-for method in [:rectangle,:trapezoid,:simpson,:romberg,:adaptive_quadrature, :gauss, :gauss_kronrod, :simpson3by8, :boole, :open_trapezoid, :milne]
+for method in [:rectangle,:trapezoid,:simpson,:romberg,:adaptive_quadrature, :gauss, :gauss_kronrod, :simpson3by8, :boole, :open_trapezoid, :milne,:qng, :qag]
   result = Integration.integrate(0,1,{:method=>method},&func)
   if result == nil
     puts method
--- benchmark/speed.rb
+++ benchmark/speed.rb
@@ -9,7 +9,7 @@ puts "Benchmarking with #{iterations} iterations"
 
 # put the function to be benchmarked here
 func = lambda{|x| x}
-for method in [:rectangle,:trapezoid,:simpson,:romberg, :adaptive_quadrature, :gauss, :gauss_kronrod, :simpson3by8, :boole, :open_trapezoid, :milne]
+for method in [:rectangle,:trapezoid,:simpson,:romberg, :adaptive_quadrature, :gauss, :gauss_kronrod, :simpson3by8, :boole, :open_trapezoid, :milne, :qng, :qag]
 	Benchmark.bm(25) do |bm|
 	  bm.report(method.to_s) do
 	    iterations.times do
